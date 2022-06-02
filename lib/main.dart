947ca2efe40d28694648b95bdae6f41198386cc8
Thomas Byr <tbouyer2000@gmail.com>
2022-06-02 10:16:23 +0200

    gala tps esbs - v1.0.0+1 [stash]

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Gala TPS ESBS',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
